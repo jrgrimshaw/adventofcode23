688177891d283b2dcef7a981d6b601ff2d8e9399
James Grimshaw <jamesrichardgrimshaw@gmail.com>
2023-12-08 14:31:51 +0000

    fixed day7pt2

--- day07/part2.py
+++ day07/part2.py
@@ -10,7 +10,7 @@ def main():
   ranked_hands = []
 
   for [hand, bid] in hands:
-    ranked_hands.append({"type": get_type_rank(hand), "hand": resort_hand(hand), "bid": bid})
+    ranked_hands.append({"type": get_type_rank(hand), "hand": hand, "bid": bid})
 
   ranked_hands = sorted(ranked_hands, key=lambda d: d["type"])
   ranked_hands = sort_same_types(ranked_hands)
@@ -40,13 +40,6 @@ def get_type_rank(hand):
   else:
     return 1
   
-def resort_hand(hand):
-    def custom_sort(char):
-      order = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
-      return order.index(char)
-
-    return ''.join(sorted(hand, key=custom_sort))
-  
 def better_hand_of_same_type(hand1, hand2):
   cards = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
 
@@ -54,12 +47,11 @@ def better_hand_of_same_type(hand1, hand2):
     return 0
   
   for i in range(5):
-    if cards.index(hand1[i]) < cards.index(hand2[i]):
-      return 1
-    elif cards.index(hand1[i]) > cards.index(hand2[i]):
-      return -1
-    else:
-      return 0
+    if hand1[i] != hand2[i]:
+      if cards.index(hand1[i]) < cards.index(hand2[i]):
+        return 1
+      elif cards.index(hand1[i]) > cards.index(hand2[i]):
+        return -1
 
 def sort_same_types(ranked_hands):
   for passnum in range(len(ranked_hands)-1,0,-1):
@@ -83,15 +75,16 @@ def make_hand_dict(hand):
   hand_dict = dict(sorted(hand_dict.items(), key=functools.cmp_to_key(better_hand_of_same_type), reverse=True))
   hand_dict = dict(sorted(hand_dict.items(), key=lambda x: x[1], reverse=True))
 
-  print(hand_dict)
-
   if "J" in hand_dict:
     for key in hand_dict:
       if key != "J":
         hand_dict[key] += hand_dict["J"]
         break
+    
+    if list(hand_dict.keys()) != ["J"]:
+      hand_dict.pop("J")
   
-  return list(hand_dict.values())
+  return list(sorted(hand_dict.values(), reverse=True))
   
 def five_of_kind(hand):
   hand_dict = make_hand_dict(hand)
@@ -99,14 +92,10 @@ def five_of_kind(hand):
 
 def four_of_kind(hand):
   hand_dict = make_hand_dict(hand)
-  if hand_dict[0] == 4:
-    print(hand)
   return hand_dict[0] == 4
 
 def full_house(hand):
   hand_dict = make_hand_dict(hand)
-  if hand_dict[0] == 3 and hand_dict[1] == 2:
-    print(hand)
   return hand_dict[0] == 3 and hand_dict[1] == 2
 
 def three_of_kind(hand):
